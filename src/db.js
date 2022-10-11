8eec7e1255d27bee8be275c859aafa67b515f1cb
Bhuwan Chandra Pandey <bp.pandey7@gmail.com>
2022-10-11 18:23:54 +0530

    DB JS [BP]

---
+++ src/db.js
@@ -0,0 +1,10 @@
+const gitHub = {
+    baseURL: "https://api.github.com/graphql",
+    userName: "bhuwanpandey7",
+    headers: {
+        "Content-Type": "applicationn/json",
+        Authorization: "bearer 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"
+    }
+}
+
+export default gitHub;
